a11fd7ce0edf455683f1812eff768c6b978e2278
Martin Montenegro <martin.92m@gmail.com>
2018-02-06 11:04:08 -0300

    Add repository and tag to remove command

--- vars/deleteDockerImages.groovy
+++ vars/deleteDockerImages.groovy
@@ -4,7 +4,7 @@ import com.wolox.*;
 def call(ProjectConfiguration projectConfig) {
     def reference = projectConfig.dockerConfiguration.reference();
     try {
-        sh "docker images --filter 'reference=${reference}*' --format \"{{.Tag}} {{.ID}}\" | sort -n | sed '\$d' | awk '{ print \$2 }' | xargs --no-run-if-empty docker rmi -f"
+        sh "docker images --filter 'reference=${reference}*' --format \"{{.Tag}} {{.Repository}}:{{.Tag}}\" | sort -n | sed '\$d' | awk '{ print \$2 }' | xargs --no-run-if-empty docker rmi"
 
     } catch(ignored) {
         // this would make the entire popeline fail. We don't want that
