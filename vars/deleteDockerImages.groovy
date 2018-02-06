6762bb4e71876b2f3b0ea2e9f90ecf011e97dc5b
Martin Montenegro <martin.92m@gmail.com>
2018-02-06 10:50:52 -0300

    Add -f option to docker rmi

--- vars/deleteDockerImages.groovy
+++ vars/deleteDockerImages.groovy
@@ -4,7 +4,7 @@ import com.wolox.*;
 def call(ProjectConfiguration projectConfig) {
     def reference = projectConfig.dockerConfiguration.reference();
     try {
-        sh "docker images --filter 'reference=${reference}*' --format \"{{.Tag}} {{.ID}}\" | sort -n | sed '\$d' | awk '{ print \$2 }' | xargs --no-run-if-empty docker rmi"
+        sh "docker images --filter 'reference=${reference}*' --format \"{{.Tag}} {{.ID}}\" | sort -n | sed '\$d' | awk '{ print \$2 }' | xargs --no-run-if-empty docker rmi -f"
 
     } catch(ignored) {
         // this would make the entire popeline fail. We don't want that
