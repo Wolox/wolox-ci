44d0853610d061b8fc8fe3302e906be83e7481c9
Matias De Santi <matias.desanti@wolox.com.ar>
2018-03-20 15:07:49 -0300

    Adds ansi color plugin

--- vars/base.groovy
+++ vars/base.groovy
@@ -5,7 +5,9 @@ def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
     return { variables ->
 
         withEnv(projectConfig.environment) {
-            nextClosure(variables)
+            wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+                nextClosure(variables)
+            }
         }
     }
 }
