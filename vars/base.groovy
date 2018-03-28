fb42e25cf9177c481a60798ac4a399eed6ff70f4
Matias De Santi <matias.desanti@wolox.com.ar>
2018-03-28 17:29:53 -0300

    Adds timeout

--- vars/base.groovy
+++ vars/base.groovy
@@ -3,10 +3,12 @@ import com.wolox.*;
 
 def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
     return { variables ->
-
-        withEnv(projectConfig.environment) {
-            wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-                nextClosure(variables)
+        def timeout = projectConfig.environment.TIMEOUT ?: 600 // timeout 10 minutes
+        timeout(time: timeout, unit: 'SECONDS') {
+            withEnv(projectConfig.environment) {
+                wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+                    nextClosure(variables)
+                }
             }
         }
     }
