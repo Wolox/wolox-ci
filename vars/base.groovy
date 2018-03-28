b3a11f6cc69cc666fa821364640ce7d82c9b5083
Matias De Santi <matias.desanti@wolox.com.ar>
2018-03-28 17:32:06 -0300

    Fixes typop

--- vars/base.groovy
+++ vars/base.groovy
@@ -3,8 +3,8 @@ import com.wolox.*;
 
 def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
     return { variables ->
-        def timeout = projectConfig.environment.TIMEOUT ?: 600 // timeout 10 minutes
-        timeout(time: timeout, unit: 'SECONDS') {
+        def timeoutTime = projectConfig.env.TIMEOUT ?: 600 // timeout 10 minutes
+        timeout(time: timeoutTime, unit: 'SECONDS') {
             withEnv(projectConfig.environment) {
                 wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
                     nextClosure(variables)
