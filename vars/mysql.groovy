79ad98bcf88fd13f77f245ad1728ecc5b83ad0e4
Martin Montenegro <martin.92m@gmail.com>
2018-07-30 10:27:20 -0400

    Fix comments

--- vars/mysql.groovy
+++ vars/mysql.groovy
@@ -4,7 +4,7 @@ import com.wolox.*;
 def call(ProjectConfiguration projectConfig, def version, def nextClosure) {
     return { variables ->
         def dbPassword = 'my-secret-pw'
-        /* Build mssql image */
+        /* Build mysql image */
         docker.image("mysql:${version}").withRun("-e \"MYSQL_ROOT_PASSWORD=${dbPassword}\"") { db ->
             withEnv(['DB_USERNAME=root', "DB_PASSWORD=${dbPassword}", "DB_HOST=db", "DB_PORT=3306"]) {
                 variables.db = db;
