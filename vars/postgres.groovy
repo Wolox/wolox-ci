4f97387173f3fb88489829e854b2f0609f14c8a8
mdesanti <matias.desanti@wolox.com.ar>
2019-05-13 18:35:20 -0300

    Update postgres.groovy

--- vars/postgres.groovy
+++ vars/postgres.groovy
@@ -7,6 +7,7 @@ def call(ProjectConfiguration projectConfig, def version, def nextClosure) {
         docker.image("postgres:${version}").withRun() { db ->
             withEnv(['DB_USERNAME=postgres', 'DB_PASSWORD=', "DB_HOST=localhost", "DB_PORT=5432"]) {
                 variables.db = db;
+                sh 'while ! psql -h localhost -U $DB_USERNAME -c "select 1"; do sleep 1; done
                 nextClosure(variables)
             }
         }
