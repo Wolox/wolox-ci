2d936319c6de443e592fcab24e087cca370c4c08
Santiago Acuña <santiago.acuna@wolox.com.ar>
2019-07-23 17:07:26 -0300

    Change image source. There was a typo

--- vars/postgis.groovy
+++ vars/postgis.groovy
@@ -4,7 +4,7 @@ import com.wolox.*;
 def call(ProjectConfiguration projectConfig, def version, def nextClosure) {
     return { variables ->
         /* Build postgres image with postgis support */
-        docker.image("mdillion/postgis:${version}").withRun() { db ->
+        docker.image("mdillon/postgis:${version}").withRun() { db ->
             withEnv(['DB_USERNAME=postgres', 'DB_PASSWORD=', "DB_HOST=db", "DB_PORT=5432"]) {
                 variables.db = db;
                 nextClosure(variables)
