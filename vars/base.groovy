2dce1a831202336c59a229650a56980b2a80315a
Manuel Tuero <manuel.tuero@wolox.com.ar>
2019-10-24 16:06:59 -0300

    chore: format code

--- vars/base.groovy
+++ vars/base.groovy
@@ -2,15 +2,16 @@
 import com.wolox.*;
 
 def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
-    return { variables ->
-        timeout(time: projectConfig.timeout, unit: 'SECONDS') {
-          withCredentials([string(credentialsId: 'client_secret_json', variable: 'TOKEN')]) {
-            withEnv(projectConfig.environment << "SPREADSHEET_CLIENT_SECRET=$TOKEN") {
-              wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-                nextClosure(variables)
-							}
-            }
-          }
-      	}
+ return {
+  variables ->
+  timeout(time: projectConfig.timeout, unit: 'SECONDS') {
+   withCredentials([string(credentialsId: 'client_secret_json', variable: 'TOKEN')]) {
+    withEnv(projectConfig.environment << "SPREADSHEET_CLIENT_SECRET=$TOKEN") {
+     wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+      nextClosure(variables)
+     }
     }
+   }
+  }
+ }
 }
