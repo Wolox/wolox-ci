0bb1dca633c46f546c2924ee71ae0eb69c0488e0
mdesanti <matias.desanti@wolox.com.ar>
2019-10-24 16:39:25 -0300

    Update base.groovy

--- vars/base.groovy
+++ vars/base.groovy
@@ -5,7 +5,7 @@ def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
  return {
   variables ->
   timeout(time: projectConfig.timeout, unit: 'SECONDS') {
-   withCredentials([string(credentialsId: 'client_secret_json', variable: 'TOKEN')]) {
+   withCredentials([file(credentialsId: 'client_secret_json', variable: 'TOKEN')]) {
     withEnv(projectConfig.environment << "SPREADSHEET_CLIENT_SECRET=$TOKEN") {
      wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
       nextClosure(variables)
