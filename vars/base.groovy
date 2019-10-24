a7a8e40e7e5a582eaea5d87740d41a6b1850c865
Manuel Tuero <manuel.tuero@wolox.com.ar>
2019-10-24 15:18:49 -0300

    chore: add withCredentials sentence to test

--- vars/base.groovy
+++ vars/base.groovy
@@ -4,7 +4,12 @@ import com.wolox.*;
 def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
     return { variables ->
         timeout(time: projectConfig.timeout, unit: 'SECONDS') {
-            withEnv(projectConfig.environment) {
+            withCredentials([
+              file(credentialsId: 'SPREADSHEET_CLIENT_SECRET', variable: 'spreadsheet_client_secret')]) {
+                sh "cp \$client_secret /client_secret.json"
+            }
+
+            withEnv(projectConfig.environment << "FOO=BAR") {
                 wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
                     nextClosure(variables)
                 }
