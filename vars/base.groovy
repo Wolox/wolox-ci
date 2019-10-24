3068f6aae18c9959b165e7a60b78d97dbc275f0e
Manuel Tuero <manuel.tuero@wolox.com.ar>
2019-10-24 16:03:34 -0300

    refactor: modify nested sentences and change credentialsId

--- vars/base.groovy
+++ vars/base.groovy
@@ -4,16 +4,13 @@ import com.wolox.*;
 def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
     return { variables ->
         timeout(time: projectConfig.timeout, unit: 'SECONDS') {
-            withCredentials([
-              file(credentialsId: 'SPREADSHEET_CLIENT_SECRET', variable: 'spreadsheet_client_secret')]) {
-                sh "cp \$client_secret /client_secret.json"
+          withCredentials([string(credentialsId: 'client_secret_json', variable: 'TOKEN')]) {
+            withEnv(projectConfig.environment << "SPREADSHEET_CLIENT_SECRET=$TOKEN") {
+              wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+                nextClosure(variables)
+							}
             }
-
-            withEnv(projectConfig.environment << "FOO=BAR") {
-                wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-                    nextClosure(variables)
-                }
-            }
-        }
+          }
+      	}
     }
 }
