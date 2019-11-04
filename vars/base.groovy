a6abeb835ce30c02b3545b523493a8e54a8baa20
Joaquin Battilana <battilanajn@gmail.com>
2019-11-04 17:07:22 -0300

    deleted last change with credentials

--- vars/base.groovy
+++ vars/base.groovy
@@ -3,15 +3,12 @@ import com.wolox.*;
 
 def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
  return {
-  variables ->
-  timeout(time: projectConfig.timeout, unit: 'SECONDS') {
-   withCredentials([file(credentialsId: 'client_secret_json', variable: 'TOKEN')]) {
+  variables -> timeout(time: projectConfig.timeout, unit: 'SECONDS') {
     withEnv(projectConfig.environment << "SPREADSHEET_CLIENT_SECRET=$TOKEN") {
      wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
       nextClosure(variables)
      }
     }
-   }
   }
  }
 }
