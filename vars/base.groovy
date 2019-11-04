9a788050d3ff739f72f3799637f4b86189e21902
Joaquin Battilana <battilanajn@gmail.com>
2019-11-04 17:16:53 -0300

    fixed, now in real first state

--- vars/base.groovy
+++ vars/base.groovy
@@ -2,13 +2,13 @@
 import com.wolox.*;
 
 def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
- return {
-  variables -> timeout(time: projectConfig.timeout, unit: 'SECONDS') {
-    withEnv(projectConfig.environment << "SPREADSHEET_CLIENT_SECRET=$TOKEN") {
-     wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
-      nextClosure(variables)
-     }
+    return { variables ->
+        timeout(time: projectConfig.timeout, unit: 'SECONDS') {
+            withEnv(projectConfig.environment) {
+                wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+                    nextClosure(variables)
+                }
+            }
+        }
     }
-  }
- }
 }
