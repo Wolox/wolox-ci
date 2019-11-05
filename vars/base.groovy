033dc7cbc8211cb05cd41f5f69af52a36d95ab43
Joaquin Battilana <battilanajn@gmail.com>
2019-11-05 18:15:23 -0300

    trying to cp from library

--- vars/base.groovy
+++ vars/base.groovy
@@ -5,6 +5,7 @@ def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
     return { variables ->
         timeout(time: projectConfig.timeout, unit: 'SECONDS') {
             withCredentials([file(credentialsId: 'client_secret_json', variable: 'FILE')]){
+                cp "\$FILE /home/node/project-health-checker"
                 withEnv(projectConfig.environment) {
                     wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
                         nextClosure(variables)
