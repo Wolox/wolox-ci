97d31f88a6045e76ed1f3cd6d5cb00b9bcb0c2fc
Joaquin Battilana <battilanajn@gmail.com>
2019-11-05 16:30:41 -0300

    deleted use

--- vars/base.groovy
+++ vars/base.groovy
@@ -5,7 +5,6 @@ def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
     return { variables ->
         timeout(time: projectConfig.timeout, unit: 'SECONDS') {
             withCredentials([file(credentialsId: 'client_secret_json', variable: 'FILE')]){
-                sh 'use $FILE'
                 withEnv(projectConfig.environment) {
                     wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
                         nextClosure(variables)
