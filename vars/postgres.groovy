5d51cf64e5998612a7844414b6a494ec9b69b99c
mdesanti <matias.desanti@wolox.com.ar>
2020-02-17 14:17:25 -0300

    Update postgres.groovy

--- vars/postgres.groovy
+++ vars/postgres.groovy
@@ -4,10 +4,9 @@ import com.wolox.*;
 def call(ProjectConfiguration projectConfig, def version, def nextClosure) {
     return { variables ->
         /* Build postgres image */
-        docker.image("postgres:${version}").withRun() { db ->
+        docker.image("postgres:12.1").withRun() { db ->
             withEnv(['DB_USERNAME=postgres', 'DB_PASSWORD=', "DB_HOST=db", "DB_PORT=5432"]) {
                 variables.db = db;
-                sleep(time: 90, unit: "SECONDS")
                 nextClosure(variables)
             }
         }
